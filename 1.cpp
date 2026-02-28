23b0e8eb19dfee4d8296503b5e0dccc21a8823da
Joseph AyoOluwa Owolabi <joseyowolabi@gmail.com>
2026-02-28 19:50:15 +0200

    Implement brightness adjustment for image processing

--- 1.cpp
+++ 1.cpp
@@ -8,11 +8,17 @@ int main() {
 
   int w = img.GetWidth();
   int h = img.GetHeight();
+  int britness = 10;
 
   for (int y = 0; y < h; y++) {
     for (int x = 0; x < w; x++) {
       RGBA pixel = img(x, y);
-      img(x, y) = RGBA(255 - pixel.r, 255 - pixel.g, 255 - pixel.b, pixel.a);
+      if (pixel.r == 0 && pixel.g == 0 && pixel.b == 0 || pixel.a == 0)
+        continue;
+      int r = car(pixel.r + britness, 255);
+      int g = car(pixel.g + britness, 255);
+      int b = car(pixel.b + britness, 255);
+      img(x, y) = RGBA(r, g, b);
     }
   }
 
