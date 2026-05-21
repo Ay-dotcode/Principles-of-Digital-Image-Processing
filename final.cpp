46029ed57d5bcfce0cbf6b463ff6f688533a0599
Joseph AyoOluwa Owolabi <joseyowolabi@gmail.com>
2026-05-21 12:39:45 +0300

    final exam code

---
+++ final.cpp
@@ -0,0 +1,40 @@
+#include "image.h"
+
+using namespace std;
+
+void segment(const ColorImage &img) {
+  double s = 0;
+  GrayscaleImage out(img.GetWidth(), img.GetHeight());
+
+  for (int y = 0; y < img.GetHeight(); y++) {
+    for (int x = 0; x < img.GetWidth(); x++) {
+      RGBA pixel = img(x, y);
+      double avg = double(pixel.r + pixel.g + pixel.b) / 3;
+
+      int max = pixel.r;
+      int min = pixel.r;
+      max = max > pixel.g ? max : pixel.g;
+      max = max > pixel.b ? max : pixel.b;
+      min = min < pixel.g ? min : pixel.g;
+      min = min < pixel.b ? min : pixel.b;
+
+      if (avg == 0)
+        continue;
+      s += (max - min) / avg;
+    }
+  }
+  s = s / (img.GetHeight() * img.GetWidth());
+
+  out = img;
+  for (int y = 0; y < img.GetHeight(); y++)
+    for (int x = 0; x < img.GetWidth(); x++)
+      out(x, y) = out(x, y) > s ? 255 : 0;
+  out.Save("final.png");
+}
+
+int main() {
+  ColorImage img;
+  img.Load("lena.png");
+  segment(img);
+  return 0;
+}
